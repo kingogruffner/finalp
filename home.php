54243d81b1c4ef3cb25be984093ed60dcd4db905
Hawkzyy <36340874+Hawkzyy@users.noreply.github.com>
2018-04-12 11:59:29 -0400

    updated home. Nothing new yet

---
+++ home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SlimeyArt Home</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.2.0-dist/css/bootstrap.css">
+</head>
+<body>
+	<!-- Header -->
+	<div class="pagewidth">
+    <header>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid row">
+
+                <div class="col-md-5 noshrink">
+                    <a class="navbar-brand pull-left" href="#">Slimey Art</a>
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-5">
+                    <ul class="nav navbar-nav pull-right">
+                        <li><a href="ideasubmit.html"><span class="glyphicon glyphicon-log-out"></span> Upload <article></article></a></li>
+                        <li><a href="#" id="bm"><span class="glyphicon glyphicon-star"></span> Favorite</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- ------------------------------------------Navigation Buttons----------------------------------------------------- -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="collapse navbar-collapse" id="myNavbar">
+
+                            <ul class="nav navbar-nav">
+                                <li><a href="index.html">Main</a></li>
+                                <li><a href="#">About</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div class="col-md-1">
+                    </div>
+                    <div class="col-md-3 lilpad">
+                        <form class="pull-right" action="http://www.randyconnolly.com" method="get">
+                            <div class="input-group">
+                                <input class ="form-control" name="search" type="text" placeholder="Search">
+                                <div class= "input-group-btn">
+                                    <button  type="submit" class="btn btn-primary">
+                                        <span class="glyphicon glyphicon-search"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </nav>
+
+    </header>
+</div>
+			<!--Side Menu-->
+</body>
+</html>
